e1c1c9f63094947af997fe880493bf23d70d3335
Irene Anthony <56097221+IreneRAnthony@users.noreply.github.com>
2020-10-14 17:01:22 -0700

    Add files via upload

---
+++ React/Projects/Section 15/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+import Auxilliary from '../../hoc/Auxilliary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actionTypes from '../../store/actions';
+
+class BurgerBuilder extends Component {
+    state = {
+        purchasing: false,
+        loading: false,
+        error: false
+    }
+
+    componentDidMount(){
+        axios.get('https://react-my-burger-b5afe.firebaseio.com/Ingredients.json')
+            .then(response => {
+                this.setState({ingredients: response.data});
+            })
+            .catch(error => {
+                this.setState({error: true});
+            })
+    }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey];
+        }).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        return sum > 0;
+    };
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
+    };
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    };
+
+    purchaseContinueHandler = () => {
+        this.props.history.push('/checkout');
+    }
+
+    render(){
+        const disabledInfo = {
+            ...this.props.ings
+        };
+        for(let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
+        let orderSummary = null;
+        let burger = this.state.error ? <p>Ingredients cannot be loaded.</p> : <Spinner />
+
+        if(this.props.ings){
+            burger = (
+                <Auxilliary>
+                    <Burger ingredients = {this.props.ings} />
+                    <BuildControls 
+                        ingredientAdded = {this.props.onIngredientAdded}
+                        ingredientRemoved = {this.props.onIngredientRemoved}
+                        disabled = {disabledInfo}
+                        price={this.props.price} 
+                        purchasable={this.updatePurchaseState(this.props.ings)}
+                        ordered={this.purchaseHandler}/>
+                </Auxilliary>
+                );
+            orderSummary = 
+                <OrderSummary 
+                ingredients={this.props.ings}
+                price={this.props.price}
+                purchaseCancelled={this.purchaseCancelHandler}
+                purchaseContinued={this.purchaseContinueHandler}
+                />
+        };
+
+        if(this.state.loading){
+            orderSummary = <Spinner />;
+        };
+
+        return (
+            <Auxilliary>
+                <Modal 
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}
+                >
+                    {orderSummary}
+                </Modal>
+                {burger}
+            </Auxilliary>
+        );
+    }
+};
+
+const mapStateToProps = (state) => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
+        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.DELETE_INGREDIENT, ingredientName: ingName})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
